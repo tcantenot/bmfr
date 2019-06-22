c697c357fe2750cfba15ab8b7ead2c4863c769c9
CANTENOT Thierry <thierrycantenot@gmail.com>
2019-06-22 15:34:06 +0200

    Add Cuda GPU bandwidth tests

--- bmfr/main.cpp
+++ bmfr/main.cpp
@@ -2,20 +2,24 @@
 #include "bmfr_c_opencl.hpp"
 #include "bmfr_cuda.hpp"
 #include "bmfr_cuda_opencl.hpp"
+#include "new_bmfr_cuda.hpp"
 
 #include <assert.h>
+
+#include "BandwidthTests.hpp"
 
 // CUDA 8.0 on Visual Studio 2017
 //  - Install CUDA 8.0
 //  - Copy content of C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\extras\visual_studio_integration\MSBuildExtensions
 //	  into C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\BuildCustomizations
 //  - Select Cuda 8.0 on the project right-click menu "Build Dependencies/Build Customizations..."
-int main()
+int bmfr()
 {
 	TmpData cuda_tmp_data, opencl_tmp_data;
 	//bmfr_c_opencl(opencl_tmp_data);
-	//bmfr_cuda(cuda_tmp_data);
-	bmfr_cuda_c_opencl(cuda_tmp_data, opencl_tmp_data);
+	bmfr_cuda(cuda_tmp_data);
+	new_bmfr_cuda(cuda_tmp_data);
+	//bmfr_cuda_c_opencl(cuda_tmp_data, opencl_tmp_data);
 
 #if !ENABLE_DEBUG_OUTPUT_TMP_DATA
 	return 0;
@@ -26,13 +30,13 @@ int main()
 
 	printf("\n");
 
+#if 0
 	CheckDiffFloat("normals", cuda_tmp_data.normals, opencl_tmp_data.normals);
 	CheckDiffFloat("positions", cuda_tmp_data.positions, opencl_tmp_data.positions);
-	//CheckDiffFloat("noisy_1spp", cuda_tmp_data.noisy_1spp, opencl_tmp_data.noisy_1spp);
+	CheckDiffFloat("noisy_1spp", cuda_tmp_data.noisy_1spp, opencl_tmp_data.noisy_1spp);
 	CheckDiffFloat("prev_frame_pixel_coords_buffer", cuda_tmp_data.prev_frame_pixel_coords_buffer, opencl_tmp_data.prev_frame_pixel_coords_buffer, false);
-	//CheckDiffFloat("features_buffer0", cuda_tmp_data.features_buffer0, opencl_tmp_data.features_buffer0);
+	CheckDiffFloat("features_buffer0", cuda_tmp_data.features_buffer0, opencl_tmp_data.features_buffer0);
 	
-#if 0
 	assert(cuda_tmp_data.prev_frame_bilinear_samples_validity_mask.size() == opencl_tmp_data.prev_frame_bilinear_samples_validity_mask.size());
 	for(int i = 0; i < cuda_tmp_data.prev_frame_bilinear_samples_validity_mask.size(); ++i)
 	{
@@ -50,18 +54,29 @@ int main()
 			printf("spp[%d]: %d != %d\n", i, (int)cuda_tmp_data.spp[i], (int)opencl_tmp_data.spp[i]);
 		}
 	}
-#endif
-	
-#if 0
+
 	CheckDiffFloat("min_max", cuda_tmp_data.features_min_max_buffer, opencl_tmp_data.features_min_max_buffer);
 	CheckDiffFloat("features_buffer1", cuda_tmp_data.features_buffer1, opencl_tmp_data.features_buffer1);
 	CheckDiffFloat("features_weights_buffer", cuda_tmp_data.features_weights_buffer, opencl_tmp_data.features_weights_buffer);
-
 	CheckDiffFloat("noisefree_1spp", cuda_tmp_data.noisefree_1spp, opencl_tmp_data.noisefree_1spp);
 	CheckDiffFloat("noisefree_1spp_accumulated", cuda_tmp_data.noisefree_1spp_accumulated, opencl_tmp_data.noisefree_1spp_accumulated);
 	CheckDiffFloat("noisefree_1spp_acc_tonemapped", cuda_tmp_data.noisefree_1spp_acc_tonemapped, opencl_tmp_data.noisefree_1spp_acc_tonemapped);
 #endif
+
 	CheckDiffFloat("result", cuda_tmp_data.result, opencl_tmp_data.result);
+
+	return 0;
+}
+
+int main()
+{
+	//BandwidthTests();
+	//return 0;
+
+	for(int i = 0; i < 1; ++i)
+	{
+		bmfr();
+	}
 
 	return 0;
 }
